33c02f54765e5ec34625c90dd5d57d4fdd906633
JBurr84277 <jtb84277@email.vccs.edu>
2025-04-04 22:47:38 -0400

    Update test_file.ino

--- test_file.ino
+++ test_file.ino
@@ -1,20 +1,32 @@
 #include <Servo.h> 
-#include <IRremote.h>
 // Declare the Servo pin 
 int servoPin = 28; 
 // Declare IR
-#define IR_PIN 3
-#define IR_RECV_PIN 29
+const int IR_PIN 3;
+const int IR_RECV_PIN 29;
 // Create a servo object 
 Servo Servo1; 
 int pos = 0;
 void setup() {
+Servo1.attach(servoPin);//servo
+pinMode(IR_PIN, OUTPUT);//transmitter
+pinMode(IR_RECV_PIN, INPUT);//receiver
 Serial.begin(9600);
-Servo1.attach(servoPin); 
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+int sensorValue = digitalRead(IR_RECV_PIN);  //read value from IR sensor
+  if (sensorValue == LOW) {
+    // Obstacle detected
+    digitalWrite(IR_PIN, HIGH);  // Turn on LED (optional)
+    Serial.println("Obstacle detected!");
+  } else {
+    // No obstacle
+    digitalWrite(IR_PIN, LOW);   // Turn off LED (optional)
+    Serial.println("No obstacle.");
+  }
 
 for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
